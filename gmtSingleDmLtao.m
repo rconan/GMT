fd8b134f6ab6949404eba2132ca2033469a5249b
Rodolphe Conan <rconan@mso.anu.edu.au>
2011-09-14 17:05:19 +1000

    REVERT TO A WORKING VERSION (WITHOUT TRUTH SENSING)

--- gmtSingleDmLtao.m
+++ gmtSingleDmLtao.m
@@ -1,10 +1,8 @@
 %% GMT LTAO MODELING WITH OOMAO
 % Demonstrate how to build the GMT LTAO system
 
-%%
 forceSettings = false;
-matPath = fullfile('~','Desktop','Project','ANU','GMT','mcode','mat');
-filename  = fullfile(matPath,'gmtNonSegSingleDmLtaoSettings.mat');
+filename  = '../mat/gmtSingleDmLtaoSettings.mat';
 if exist(filename,'file') && ~forceSettings
     
     fprintf('   >>> LOAD SETTINGS FROM %s ....',upper(filename))
@@ -24,7 +22,7 @@ else
     nLenslet = 50;
     nPx = nLenslet*8;
     tel = telescope(25,...
-        'obstructionRatio',0.4,...
+        'obstructionRato',0.4,...
         'fieldOfViewInArcMin',2.5,...
         'resolution',nPx,...
         'samplingTime',1/500);
@@ -32,35 +30,33 @@ else
 %         'fieldOfViewInArcMin',2.5,...
 %         'resolution',nPx,...
 %         'samplingTime',1/500);
-%     
+%%
+gmtPups = zeros([size(tel.pupil),7]);
+for k=1:7;gmtPups(:,:,k)=tel.segment{k}.pupil;end
+gmtPups = reshape(gmtPups,[],7);
+sumGmtPups = sum(gmtPups)';
+     
     %% Definition of a calibration source
     ngs = source('wavelength',photometry.Na);
     
     %% Definition of the wavefront sensor
     wfs = shackHartmann(nLenslet,nPx,0.85);
-    wfs.tag = 'LGS WFS';
-    wfs.camera.exposureTime = tel.samplingTime;
-    % Propagation of the calibration source to the WFS through the telescope
     ngs = ngs.*tel*wfs;
     wfs.INIT;
     +wfs;
     figure
-    subplot(1,2,1)
     imagesc(wfs.camera)
-    subplot(1,2,2)
-    slopesDisplay(wfs)
     
     %% Definition of the deformable mirror
     bif = influenceFunction('monotonic',50/100);
-    % Cut of the influence function
-    % figure
-    % show(bif)
     nActuator = nLenslet + 1;
     dm = deformableMirror(nActuator,...
         'modes',bif,...
         'resolution',nPx,...
         'validActuator',wfs.validActuator);
-    %%% Interaction matrix: DM/WFS calibration
+    
+    
+    %% Interaction matrix: DM/WFS calibration
     ngs = ngs.*tel;
     dmWfsCalib = calibration(dm,wfs,ngs,ngs.wavelength/2);
     dmWfsCalib.threshold = 4e5;
@@ -71,7 +67,6 @@ else
     tt = source('wavelength',photometry.K);
     % GMT Tip-Tilt IR sensor
     tipTiltWfs = gmtInfraredQuadCellDetector(tel,1/tel.samplingTime,40e-3,tt);
-    tipTiltWfs.tag = 'Tip-Tilt Sensor';
     tipTiltWfs.camera.readOutNoise = 0;
     tipTiltWfs.camera.photonNoise = false;
     tt = tt.*tel*tipTiltWfs;
@@ -81,24 +76,6 @@ else
     dmTipTiltWfsCalib = calibration(dm,tipTiltWfs,tt,tt.wavelength/4);
     dmTipTiltWfsCalib.nThresholded = 0;
     commandTipTilt = dmTipTiltWfsCalib.M;
-    
-    %% Truth Sensor
-    ttTruth = source('wavelength',photometry.H);
-    truth = shackHartmann(10,10*10,0.75);
-    truth.tag = 'Truth Sensor';
-    truth.lenslets.fieldStopSize = 5;
-    % Propagation of the calibration source to the WFS through the telescope
-    ttTruth = ttTruth.*tel*truth;
-    truth.INIT;
-    +truth;
-    figure
-    imagesc(truth.camera)
-    truth.camera.clockRate = 1/tel.samplingTime;
-    %% Interaction matrix: DM/TT sensor calibration
-    dmTruthCalib = calibration(dm,truth,ngs,ngs.wavelength/2);
-    dmTruthCalib.nThresholded = 23;
-    commandTruth = dmTruthCalib.M;
-
     %%
     lgs = source('asterism',{[6,arcsec(35),0]},'wavelength',photometry.Na,'height',90e3);
     ltaoMmse = linearMMSE(dm.nActuator,tel.D,atm,lgs,ngs,'pupil',dm.validActuator,'unit',-9);
@@ -133,7 +110,8 @@ dm.coefs = 0;
 %%
 % Propagation throught the atmosphere to the telescope
 ngs=ngs.*tel;
-lgs = lgs.*tel;
+lgs=lgs.*tel;
+tt = tt.*tel;
 %%
 % Saving the turbulence aberrated phase
 turbPhase = ngs.meanRmPhase;
@@ -151,26 +129,14 @@ ylabel(colorbar,'WFE [\mum]')
 %%
 % Closed loop integrator gain:
 loopGain = 0.5;
-nIteration = 2000;
-srcorma = sourceorama(fullfile(matPath,'gmtSingleDmLtao.h5'),[ngs;lgs(:)],nIteration*tel.samplingTime,tel,0.25);
-%% Low pass filter
-lpfTime = 1;
-alpha = lpfTime/(1+lpfTime/tel.samplingTime);
-lpfSlopes = 0;
+nIteration = 500;
+srcorma = sourceorama('../mat/gmtSingleDmLtao.h5',[ngs;lgs(:)],nIteration*tel.samplingTime,tel,0.25);
 %%
 % closing the loop
 total  = zeros(1,nIteration);
 residue = zeros(1,nIteration);
-dmCoefs   = zeros(dm.nValidActuator,1);
-dmCoefsTt = zeros(dm.nValidActuator,1);
-dmCoefsTruth = zeros(dm.nValidActuator,1);
-dm.coefs  = 0;
+dm.coefs = 0;
 ux = ones(1,length(lgs));
-wfs.rmMeanSlopes = true;
-truth.rmMeanSlopes = true;
-% truthFrame = truth.camera.exposureTime*truth.camera.clockRate;
-% hTruth = waitbar(0,'truth');
-truth.camera.frameCount = 0;
 tic
 for kIteration=1:nIteration
     % Propagation throught the atmosphere to the telescope, +tel means that
@@ -179,57 +145,44 @@ for kIteration=1:nIteration
     +srcorma;
 %     ngs=ngs.*+tel;
     tt.resetPhase = ngs.opd*tt.waveNumber;
-    ttTruth.resetPhase = ngs.opd*ttTruth.waveNumber;
     % Saving the turbulence aberrated phase
     turbPhase = ngs.meanRmPhase;
     % Variance of the atmospheric wavefront
     total(kIteration) = var(ngs);
     % Propagation to the WFS
     ngs=ngs*dm;
+    % piston circus
+    ngs.phase = -reshape( gmtPups*((gmtPups'*ngs.phase(:))./sumGmtPups) , tel.resolution , tel.resolution );
     lgs = lgs*dm*wfs;
    % Variance of the residual wavefront
     residue(kIteration) = var(ngs);
     % Computing the DM residual coefficients
-    %     residualDmCoefs = M*wfs.slopes(:);
-    
+    meanRmWfsSlopes = bsxfun(@minus,wfs.slopes,mean(wfs.slopes));
+    %     residualDmCoefs = M*meanRmWfsSlopes(:);
     % -.- TT sensing
     tt = tt*dm*tipTiltWfs;
     residualDmTtCoefs = commandTipTilt*tipTiltWfs.slopes;
-    dmCoefsTt = dmCoefsTt - loopGain*residualDmTtCoefs;
-    % TT sensing -.-
-    
-    % -.- Truth sensing
-    ttTruth = ttTruth*dm*truth;
-%     waitbar(truth.camera.frameCount/truthFrame)
-    residualDmTruthCoefs = commandTruth*truth.slopes;
-    dmCoefsTruth = dmCoefsTruth - residualDmTruthCoefs;
-    % Truth sensing -.-
-    
+    %     % TT sensing -.-
     %     % Integrating the DM coefficients
     %     dm.coefs = dm.coefs - loopGain*(residualDmCoefs + residualDmTtCoefs);
     
     % DM slopes
-    S_DM = dmWfsCalib.D*dmCoefs;
+    S_DM = dmWfsCalib.D*dm.coefs;
     % LGS full turbulence slopes estimate
-    S_LGS = wfs.slopes - S_DM*ux;
-    lpfSlopes = alpha*S_LGS + (1-alpha)*lpfSlopes;
-    S_LGS = S_LGS - lpfSlopes;
+    S_LGS = meanRmWfsSlopes-S_DM*ux;
     % on-axis full turbulence DM command tomography estimate
     C_NGS = M*S_LGS(:);
     % on-axis residual turbulence DM command estimate
-    C_res_NGS = C_NGS + dmCoefs;
+    C_res_NGS = C_NGS + dm.coefs;
     % integrator or (may be) low-pass filter (to check)
-    dmCoefs   = dmCoefs   - loopGain*C_res_NGS;
-    
-    dm.coefs  = dmCoefs + dmCoefsTt + dmCoefsTruth;
-    
+    dm.coefs = dm.coefs - loopGain*(C_res_NGS+residualDmTtCoefs);
+
     % Display of turbulence and residual phase
     set(h,'Cdata',[turbPhase,ngs.meanRmPhase]*rad2mic)
     title(ax,sprintf('#%4d/%4d',kIteration,nIteration))
     drawnow
 end
 clear srcorma
-% close(hTruth)
 toc
 %%
 % Piston removed phase variance
